994ec41c259bcada5cc87a2c9556f23eb6f1cbd2
Affan Ilham Arsyila <l200234024@student.ums.ac.id>
2025-06-12 19:27:22 +0700

    pushing etc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // biar bisa diakses dari jaringan lain
+    host: true,
     port: 5173,
     strictPort: true,
-    origin: "https://5efe-157-10-8-219.ngrok-free.app", // opsional untuk HMR via proxy
+    origin: "https://7548-182-253-89-51.ngrok-free.app",
   },
 });
